f8b022176fa342a6ad2bf3b678c1f7747b50ee76
Felipe Gabriel <lipebiel35@gmail.com>
2025-02-22 22:19:42 -0300

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# ATUALIZAÇÕES DO PROJETO - Versão 1.0.1
+
+## [1.0.1] - Atualização 22/02/2025
+Correção do erro que impedia que o jogo terminasse se o termo houvesse mais que uma palavra
--- script.js
+++ script.js
@@ -1,78 +1,98 @@
-var palavraFiltro = null
-var palavra = null
-var vidas = 5
+var palavraFiltro = null;
+var palavra = null;
+var vidas = 5;
 
-function prepararJogo(){
-    document.getElementById("teclado").style.display= "block"
-    vidas = 5
-    document.getElementById("forca").src = `imgs/forca-5-vidas.png`
-    var tema = document.getElementById("temas").value
-    document.getElementById("placeholderPalavra").innerHTML = ""
+function prepararJogo() {
+    document.getElementById("teclado").style.display = "block";
+    vidas = 5;
+    document.getElementById("forca").src = `imgs/forca-5-vidas.png`;
+    
+    var tema = document.getElementById("temas").value;
+    document.getElementById("placeholderPalavra").innerHTML = "";
 
-    var tecla = document.querySelectorAll(".tecla")
-    for (let index = 0; index < tecla.length; index++) {
-        tecla[index].disabled = false   
-        tecla[index].classList.remove("certo")
-    }
-    gerarPalavra(tema)
+    var teclas = document.querySelectorAll(".tecla");
+    teclas.forEach(tecla => {
+        tecla.disabled = false;
+        tecla.classList.remove("certo");
+    });
+
+    gerarPalavra(tema);
 }
 
-function gerarPalavra(tema){
+function gerarPalavra(tema) {
     fetch("https://raw.githubusercontent.com/Felipe-Gabriel-Souza-Goncalves/api_jogo-da-forca/refs/heads/main/palavras.json")
-    .then(response => response.json())
-    .then(dados =>{
-        var index = Math.floor(Math.random()*dados.temas[tema].length)
-        palavra = dados.temas[tema][index].palavra.split("")
-        palavraFiltro = dados.temas[tema][index].palavra_filtro.split("")
+        .then(response => response.json())
+        .then(dados => {
+            var index = Math.floor(Math.random() * dados.temas[tema].length);
+            palavra = dados.temas[tema][index].palavra.split("");
+            palavraFiltro = dados.temas[tema][index].palavra_filtro.split("");
 
-        for(let i = 0; i<palavra.length; i++){
-            var u = document.createElement("u")
-            u.classList.add("letra")
-            u.innerHTML = "&nbsp;&nbsp;&nbsp;"
+            var placeholder = document.getElementById("placeholderPalavra");
+            placeholder.innerHTML = ""; // Limpar antes de preencher
 
-            document.getElementById("placeholderPalavra").appendChild(u)
+            for (let i = 0; i < palavra.length; i++) {
+                if (palavra[i] === " ") { // Corrigido: verificar espaço com " "
+                    const span = document.createElement("span");
+                    span.classList.add("espaco");
+                    span.innerHTML = "&nbsp;&nbsp;&nbsp;";
+                    placeholder.appendChild(span);
+                } else {
+                    const u = document.createElement("u");
+                    u.classList.add("letra");
+                    u.innerHTML = "&nbsp;&nbsp;&nbsp;";
+                    placeholder.appendChild(u);
+                }
+            }
+        })
+        .catch(error => console.error("Erro ao carregar as palavras:", error));
+}
+
+function mandarLetra(letra, button) {
+    var existeLetra = false;
+    var uElements = document.querySelectorAll("u");
+
+    var diff = 0
+    for (let i = 0; i < palavraFiltro.length; i++) {
+        if(palavraFiltro[i] == " "){
+            diff ++
+            continue
         }
-    })
-}
 
-function mandarLetra(letra, button){
-    var existeLetra = false
-    var u = document.querySelectorAll("u")
-
-    for(let l = 0; l< palavraFiltro.length; l++){
-        if(letra == palavraFiltro[l]){
-            existeLetra = true
-            u[l].innerHTML = palavra[l]
-        }
-    }
-    correcao(existeLetra, button)
-}
-
-function correcao(existeLetra, e){
-    if(existeLetra){
-        e.classList.add("certo")
-        e.disabled = true
-    } else{
-        e.disabled = true
-        vidas--
-        document.getElementById("forca").src = `imgs/forca-${vidas}-vidas.png`
-    }
-    if(vidas == 0){
-        alert("VOCê PERDEU O JOGO!")
-        document.getElementById("teclado").style.display= "none"
-    }
-    ganhar()
-}
-
-function ganhar(){
-    var u = document.querySelectorAll('u')
-    for(let i =0; i< u.length;i++){
-        if(u[i].innerHTML == "&nbsp;&nbsp;&nbsp;"){
-            return
+        if (letra.toLowerCase() === palavraFiltro[i].toLowerCase()) {
+            existeLetra = true;
+            uElements[i-diff].innerHTML = palavra[i];
         }
     }
 
-    alert("PARABENS!!!" + "\nVidas restantes: " + vidas)
-    document.getElementById("teclado").style.display= "none"
+    correcao(existeLetra, button);
+}
 
+function correcao(existeLetra, button) {
+    if (existeLetra) {
+        button.classList.add("certo");
+        button.disabled = true;
+    } else {
+        button.disabled = true;
+        vidas--;
+        document.getElementById("forca").src = `imgs/forca-${vidas}-vidas.png`;
+    }
+
+    if (vidas === 0) {
+        alert("VOCÊ PERDEU O JOGO!");
+        document.getElementById("teclado").style.display = "none";
+    }
+
+    ganhar();
+}
+
+function ganhar() {
+    var uElements = document.querySelectorAll("u");
+    for (let i = 0; i < uElements.length; i++) {
+        if (uElements[i].innerHTML === "&nbsp;&nbsp;&nbsp;") {
+            return; // Se ainda houver espaço em branco, o jogo continua
+        }
+    }
+
+    alert("PARABÉNS!!!\nVidas restantes: " + vidas);
+    document.getElementById("teclado").style.display = "none";
 }
